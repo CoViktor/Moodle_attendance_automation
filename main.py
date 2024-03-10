7675541849816ac748273efac99a10ca56ed7586
Viktor Cosaert <viktor.cosaert@gmail.com>
2024-03-10 12:54:07 +0100

    login handling complete, waiting for button to continue

--- main.py
+++ main.py
@@ -1,21 +1,62 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+import os
+import time
 
+# Setup
 driver = webdriver.Edge()
-
 url_attendance = 'https://moodle.becode.org/mod/attendance/view.php?id=90'
-url_login = "https://moodle.becode.org/login/index.php"
 
+
+def get_credentials():
+    if os.path.exists('./credentials.txt'):
+        with open('./credentials.txt', 'r') as credentials:
+            login= credentials.readline().strip()
+            pw = credentials.readline().strip()
+    else:
+        login= input('No credentials found. Please give your login name to be written in the credentials file for future reference: ')
+        pw = input('Please give your password to be written in the credentials file for future reference: ')
+        with open('./credentials.txt', 'w') as credentials:
+            credentials.write(login + '\n')
+            credentials.write(pw)
+    return login, pw
+
+
+def login_handling():
+    login, password = get_credentials()
+    # Locating input fields
+    login_input = driver.find_element("xpath", '//input[@id="username"]')
+    password_input = driver.find_element("xpath", '//input[@id="password"]')
+    # Clearing and filling input fields
+    login_input.clear()
+    login_input.send_keys(login)
+    password_input.clear()
+    password_input.send_keys(password)
+    # Finalizing the login & waiting a second
+    password_input.send_keys(Keys.RETURN)
+    time.sleep(5)
+
+
+# Starting driver
 driver.get(url_attendance)
+# Check if 'login' is in the URL
+if 'login' in driver.current_url:
+    login_handling()
 
-# Gaat eerst naar login 
-# -> name & pass not saved -> steek in aparte file & zet die in gitignore
+# Locate & click attendance button
+# attendance_button = driver.find_element(By.XPATH, "")
+# attendance_button.click()
 
+# close the driver after use
+driver.quit()
 
-# Zoek buttons om te klikken
+# ----------------------------- CLEANUP & UPGRAGE ---------------------------------------
 
+# Put everything in utils & define functions to import here -> mind file pathing
 
-driver.close()
+# Clean code & generate docstrings
 
-
+# Updates / Upgrades in readme:
+# check if headless works too -> maybe javascript needs to load
+# foreseen issues:id=90 of url could change
+# check if works on different dates 
