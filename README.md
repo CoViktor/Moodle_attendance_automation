ed28db86a1ee720a7fc1daab80ac64738190feda
Viktor Cosaert <viktor.cosaert@gmail.com>
2024-03-10 16:40:59 +0100

    updated readme & main script

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# Moodle_attendance_automation
+# Automated Moodle Attendance Logger 🤖
+[![forthebadge made-with-python](https://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+
+## 🏢 Description
+
+As part of the AI trainee program at BeCode.org, attendance is mandatory and tracked twice each day. To challenge myself and learn more about Selenium, I've automated my attendance logging. This script runs at the required times each day, using Selenium for web interaction, and includes a feature to skip holidays. 
+
+![Attendance Automation](image.png)
+## 📦 Repo structure
+```
+.
+├── .gitignore
+├── credentials.txt (created on first run)
+├── main.py
+├── README.md
+└── requirements.txt
+```
+
+## 🚧 Installation 
+
+1. Clone the repository to your local machine.
+
+    ```
+    git clone https://github.com/CoViktor/Moodle_attendance_automation.git
+    ```
+
+2. Navigate to the project directory and install the required dependencies:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+## ✔️ Usage 
+
+This script automates the process of logging attendance on the BeCode platform. It checks the current date against a list of predefined holidays and logs in to mark attendance if it's not a holiday.
+
+Ensure credentials.txt is set up by running the script once. If the file doesn't exist, the script will prompt you to enter your credentials, which will be saved for future use.
+
+Schedule the script to run at the required logging times using Task Scheduler or a similar tool.
+
+Here's a snippet from main.py demonstrating the core functionality:
+
+```python
+def get_credentials():
+
+def login_handling():
+
+driver.get(url_attendance)
+if 'login' in driver.current_url:
+    login_handling()
+attendance_button = driver.find_element(By.XPATH, "_")
+attendance_button.click()
+
+driver.quit()
+```
+## ⏱️ Timeline
+This project took an afternoon to complete.
+
+## 🔧 Updates & Upgrades
+### Recent Updates
+- **11/03/24**: Added pathing to the disappearing attendance button.
+
+### Planned Upgrades
+- **Date-time dependency**: Due to the nature of Moodle, the pathing to attendance button and the url might change depending on the date. This needs to be checked out and will be updated if necessary.
+
+## 📌 Personal Situation
+This project was undertaken as a learning challenge during my AI Bootcamp at [BeCode.org](https://becode.org/). It has been a valuable experience in automating real-world tasks and learning to use Selenium for web automation.
+
+Connect with me on [LinkedIn](https://www.linkedin.com/in/viktor-cosaert/).
--- main.py
+++ main.py
@@ -1,28 +1,58 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import os
 import time
+from datetime import datetime
 
-# Setup
+# Checking if it's not a day I don't have to attend
+holidays = ['2024-03-22', '2024-04-01', '2024-04-02', '2024-04-03',
+            '2024-04-04', '2024-04-05', '2024-05-01', '2024-04-09',
+            '2024-07-05', '2024-07-08', '2024-07-09', '2024-07-10', 
+            '2024-07-11', '2024-07-12', '2024-08-15']
+today = datetime.now().strftime('%Y-%m-%d')
+if today in holidays:
+    print("Today is a holiday. Exiting...")
+    exit()
+
 driver = webdriver.Edge()
 url_attendance = 'https://moodle.becode.org/mod/attendance/view.php?id=90'
 
 
 def get_credentials():
-    if os.path.exists('./credentials.txt'):
-        with open('./credentials.txt', 'r') as credentials:
+    '''Retrieves login credentials from a local 'credentials.txt' file.
+    
+    If the 'credentials.txt' file does not exist, prompts the user to enter their login name and password,
+    which are then saved to 'credentials.txt' for future use.
+    
+    Returns:
+        tuple: A tuple containing the login name and password.
+    '''
+    if os.path.exists('credentials.txt'):
+        with open('credentials.txt', 'r') as credentials:
             login= credentials.readline().strip()
             pw = credentials.readline().strip()
     else:
         login= input('No credentials found. Please give your login name to be written in the credentials file for future reference: ')
         pw = input('Please give your password to be written in the credentials file for future reference: ')
-        with open('./credentials.txt', 'w') as credentials:
+        with open('credentials.txt', 'w') as credentials:
             credentials.write(login + '\n')
             credentials.write(pw)
     return login, pw
 
 
 def login_handling():
+    '''   
+    Handles the login process for a web page using Selenium.
+    
+    This function retrieves credentials using the get_credentials function, locates the login and password input fields
+    on the current page using their XPath, clears any existing text in those fields, inputs the retrieved credentials,
+    and submits the form. It then pauses execution for 5 seconds to allow for page loading or redirection.
+    
+    Requires:
+        - A global 'driver' variable that represents an active Selenium WebDriver instance.
+        - The 'get_credentials' function to retrieve user credentials.
+    '''
     login, password = get_credentials()
     # Locating input fields
     login_input = driver.find_element("xpath", '//input[@id="username"]')
@@ -37,26 +67,15 @@ def login_handling():
     time.sleep(5)
 
 
+# Your script logic here
 # Starting driver
 driver.get(url_attendance)
 # Check if 'login' is in the URL
 if 'login' in driver.current_url:
     login_handling()
-
 # Locate & click attendance button
 # attendance_button = driver.find_element(By.XPATH, "")
 # attendance_button.click()
 
 # close the driver after use
 driver.quit()
-
-# ----------------------------- CLEANUP & UPGRAGE ---------------------------------------
-
-# Put everything in utils & define functions to import here -> mind file pathing
-
-# Clean code & generate docstrings
-
-# Updates / Upgrades in readme:
-# check if headless works too -> maybe javascript needs to load
-# foreseen issues:id=90 of url could change
-# check if works on different dates 
